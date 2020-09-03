6c31aa7caf3a47554d1c7a93fa428a98ce7f6201
muhsin61 <muhsin_61_67@hotmail.com>
2020-09-03 22:16:47 +0300

    card flip

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import FlipCard from 'react-native-flip-card';
 
 export default function App() {
+  const [text,setText] = useState("");
+  const [text2,setText2] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Uygulama çalışıyor</Text>
+      <TextInput placeholder="add word" onChangeText={text => setText(text)} defaultValue={text}/>
+      <TextInput placeholder="add word" onChangeText={text => setText2(text)} defaultValue={text2}/>
+      <Text>{text}</Text>
+      <FlipCard
+        style={styles.card}
+        friction={6}
+        perspective={1000}
+        flipHorizontal={true}
+        flipVertical={false}
+        flip={false}
+        clickable={true}
+        onFlipEnd={(isFlipEnd) => { console.log('isFlipEnd', isFlipEnd) }}
+      >
+        {/* Face Side */}
+        <View style={styles.card1}>
+          <Text>The Face</Text>
+        </View>
+        {/* Back Side */}
+        <View style={styles.card2}>
+          <Text>The Back</Text>
+        </View>
+      </FlipCard>
+
+      {/*<StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -15,6 +41,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardContainer: {
+    width: 320,
+    height: 470,
+  },
+  card: {
+    flex: 1,
+    marginTop: 10,
+    width: 100,
+    height: 0,
+    borderRadius: 20,
+    backgroundColor: '#FE474C',
+    shadowColor: 'rgba(0,0,0,0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.5,
+  },
+  card1: {
+    flex: 1,
+    left:0,
+    backgroundColor: '#FE474C',
+    //backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card2: {
+    flex: 1,
+    left:0,
+    backgroundColor: '#FEB12C',
+    //backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
